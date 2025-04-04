558b8905169e52d755b88cd69f0d4d2d1230f17b
Peyman <hassani.peyman627@gmail.com>
2025-04-04 17:44:19 +0330

    Add basic root route setup

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/root.html"))
+	data := struct {
+		Title string
+	}{
+		Title: "Short URL",
+	}
+	err := tmpl.Execute(w, data)
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
-	fmt.Println("hello")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", handleRoot)
+
+	srv := http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	log.Printf("Server is running on %s", srv.Addr)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
