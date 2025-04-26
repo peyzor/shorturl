2fdf7f9cc4684caefb84b7d2ecb5dfec3e549b1b
Peyman <hassani.peyman627@gmail.com>
2025-04-26 17:43:20 +0330

    Add error handling to html form and add random seq generator

--- main.go
+++ main.go
@@ -2,50 +2,108 @@ package main
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
 	"github.com/jackc/pgx/v5"
 	"github.com/joho/godotenv"
 	"github.com/peyzor/shorturl/db"
 	"html/template"
 	"log"
+	"math/rand"
 	"net/http"
+	"strings"
 )
 
-func (conf *Config) handleHome(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("templates/home.html"))
-	data := struct {
-		Title string
-	}{
-		Title: "Short URL",
+var baseUrl = "localhost:8080/"
+var title = "Short URL"
+var shortMaxLen = 4
+
+func randSeq(length int) string {
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+	res := make([]rune, length)
+	for i := range res {
+		res[i] = letters[rand.Intn(len(letters))]
 	}
+	return string(res)
+}
+
+type HomePageData struct {
+	Title  string
+	Errors []string
+}
+
+func renderHomePage(w http.ResponseWriter, data HomePageData) {
+	tmpl := template.Must(template.ParseFiles("templates/home.html"))
+
 	err := tmpl.Execute(w, data)
 	if err != nil {
 		panic(err)
 	}
 }
 
+type ShortsPageData struct {
+	Title string
+	Url   string
+	Short string
+}
+
+func renderShortsPage(w http.ResponseWriter, data ShortsPageData) {
+	tmpl := template.Must(template.ParseFiles("templates/shorts.html"))
+
+	err := tmpl.Execute(w, data)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (conf *Config) handleHome(w http.ResponseWriter, r *http.Request) {
+	data := HomePageData{
+		Title: title,
+	}
+
+	renderHomePage(w, data)
+}
+
 func (conf *Config) handleCreateShort(w http.ResponseWriter, r *http.Request) {
 	url := r.FormValue("url")
 	if url == "" {
-		fmt.Println("url not set")
+		log.Println("error occurred: url is not set")
+
+		errors := []string{"URL must be set."}
+		data := HomePageData{
+			Title:  title,
+			Errors: errors,
+		}
+		renderHomePage(w, data)
 		return
 	}
 
-	Url, err := conf.Queries.CreateURL(r.Context(), db.CreateURLParams{
+	UrlRow, err := conf.Queries.CreateURL(r.Context(), db.CreateURLParams{
 		Url:   url,
-		Short: "xd",
+		Short: randSeq(shortMaxLen),
 	})
-
 	if err != nil {
-		fmt.Println(err)
+		uniqueConstraintViolationStartsWith := "duplicate key"
+		if strings.Contains(err.Error(), uniqueConstraintViolationStartsWith) {
+			log.Printf("error occurred: %v\n", err)
+
+			errors := []string{"An error occurred. Please try again."}
+			data := HomePageData{
+				Title:  title,
+				Errors: errors,
+			}
+			renderHomePage(w, data)
+			return
+		}
+		log.Printf("error occurred: %v\n", err)
 		return
 	}
 
-	fmt.Printf("%+v\n", Url)
-	w.Header().Add("Content-Type", "application/json")
-	data, _ := json.Marshal(Url)
-	_, _ = w.Write(data)
+	data := ShortsPageData{
+		Title: title,
+		Url:   UrlRow.Url,
+		Short: baseUrl + UrlRow.Short,
+	}
+	renderShortsPage(w, data)
 }
 
 type Config struct {
